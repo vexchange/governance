3440e19d88276c80fe769f0f84ec789f5ab16a4c
Oliver Chalk <oliverchalk245@gmail.com>
2021-11-02 00:01:28 +0100

    Config: Add vesterconfig for mainnet (deploy #1)

--- scripts/config/vesterConfig.js
+++ scripts/config/vesterConfig.js
@@ -20,25 +20,25 @@ module.exports = {
 
 	// Token amount not in 18 decimal form
 	allocations: {
-		dao: {
-			address: "", // use the timelock address
-			tokens: 50_000_000,
-		},
+		// dao: {
+		// 	address: "AFTER_DEPLOY", // use the timelock address
+		// 	tokens: 50_000_000,
+		// },
 		oliver: {
 			address: "0x6aE0a6E33688d0f3dCe513713e73DC7D99C1A00c", // oliver personal
 			tokens: 10_000_000,
 		},
 		kenneth: {
-			address: "", // kenneth personal
+			address: "0x14Fe55c8f5F83900F280a41bA6EF31042D8808D9", // kenneth personal
 			tokens: 10_000_000,
 		},
 		proxima: {
-			address: "", // proxima personal
+			address: "0xffF7f583Dc3a36e404A99BbacA84E867A38424e3", // proxima personal
 			tokens: 10_000_000,
 		},
-		team: {
-			address: "", // oliver & kenneth shared
-			tokens: 10_000_000,
-		}
+		// team: {
+		// 	address: "TBD", // oliver & kenneth shared
+		// 	tokens: 10_000_000,
+		// }
 	}
 };
