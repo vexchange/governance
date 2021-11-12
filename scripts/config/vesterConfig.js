e9be0c5cee90de09eef178205776db0d6a67f0c1
Oliver Chalk <oliverchalk245@gmail.com>
2021-11-13 00:33:41 +0100

    DEPLOY: deployVester step (DAO)

--- scripts/config/vesterConfig.js
+++ scripts/config/vesterConfig.js
@@ -20,22 +20,22 @@ module.exports = {
 
 	// Token amount not in 18 decimal form
 	allocations: {
-		// dao: {
-		// 	address: "AFTER_DEPLOY", // use the timelock address
-		// 	tokens: 50_000_000,
+		dao: {
+			address: "0x41D293Ee2924FF67Bd934fC092Be408162448f86", // use the timelock address
+			tokens: 49_764_357,
+		},
+		// oliver: {
+		// 	address: "0x6aE0a6E33688d0f3dCe513713e73DC7D99C1A00c", // oliver personal
+		// 	tokens: 10_000_000,
 		// },
-		oliver: {
-			address: "0x6aE0a6E33688d0f3dCe513713e73DC7D99C1A00c", // oliver personal
-			tokens: 10_000_000,
-		},
-		kenneth: {
-			address: "0x14Fe55c8f5F83900F280a41bA6EF31042D8808D9", // kenneth personal
-			tokens: 10_000_000,
-		},
-		proxima: {
-			address: "0xffF7f583Dc3a36e404A99BbacA84E867A38424e3", // proxima personal
-			tokens: 10_000_000,
-		},
+		// kenneth: {
+		// 	address: "0x14Fe55c8f5F83900F280a41bA6EF31042D8808D9", // kenneth personal
+		// 	tokens: 10_000_000,
+		// },
+		// proxima: {
+		// 	address: "0xffF7f583Dc3a36e404A99BbacA84E867A38424e3", // proxima personal
+		// 	tokens: 10_000_000,
+		// },
 		// team: {
 		// 	address: "TBD", // oliver & kenneth shared
 		// 	tokens: 10_000_000,
