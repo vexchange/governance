a9e0c65d80db82c7d1566e5ba6e88af7433fbee6
A.L <xenide@users.noreply.github.com>
2022-01-10 10:40:18 +0800

    fix: changed scripts to queue and execute transaction on the timelock since timelock owns factory now

--- scripts/queueSetSwapFee.js
+++ scripts/queueSetSwapFee.js
@@ -1,9 +1,11 @@
 // ES5 style
 const config = require("./config/deploymentConfig");
+const deployedAddresses = require("./config/deployedAddresses");
 const thorify = require("thorify").thorify;
 const Web3 = require("web3");
 const Factory = require(config.pathToV2FactoryJson);
 const Pair = require(config.pathToV2PairJson);
+const Timelock = require(config.pathToTimelockJson);
 const assert = require('assert');
 const readlineSync = require("readline-sync");
 
@@ -13,7 +15,7 @@ let swapFee = null;
 
 if (process.argv.length !== 5) 
 {
-    console.error("Usage: node setSwapFee.js [mainnet|testnet] [pair address] [swapFee in basis points (100 means 1%)]");
+    console.error("Usage: node queueSetSwapFee.js [mainnet|testnet] [pair address] [swapFee in basis points (100 means 1%)]");
     process.exit(1);
 } 
 else
@@ -35,15 +37,17 @@ else
 }
 
 const web3 = thorify(new Web3(), network.rpcUrl);
-web3.eth.accounts.wallet.add(config.ownerPrivateKey);
+web3.eth.accounts.wallet.add(config.deployerPrivateKey);
 
-setSwapFee = async(pairAddress, newSwapFee) =>
+queueSetSwapFee = async(pairAddress, newSwapFee) =>
 {
     try
     {
         const walletAddress = web3.eth.accounts.wallet[0].address;
         const factoryContract = new web3.eth.Contract(Factory.abi, config.v2FactoryAddress);
         const pairContract = new web3.eth.Contract(Pair.abi, pairAddress);
+        const timelockContract = new web3.eth.Contract(Timelock.abi, deployedAddresses.timelockAddress);
+
         console.log("Using wallet address:", walletAddress);
         console.log("Using RPC:", web3.eth.currentProvider.RESTHost);
 
@@ -52,20 +56,43 @@ setSwapFee = async(pairAddress, newSwapFee) =>
             let input = readlineSync.question("Confirm you want to call this on the MAINNET? (y/n) ");
             if (input != 'y') process.exit(1);
         }
-        
+
+        const value = 0;
+        const signature = "setSwapFeeForPair(address,uint256)";
+        const data = web3.eth.abi.encodeParameters(["address","uint256"], [pairAddress,swapFee]);
+
+        // Add some buffer to the delay 
+        const eta = parseInt(timestamp + config.timelockDelay * 1.05);
+
         console.log("Adjusting swapFee for pair at address", pairAddress);
         console.log("Old swap fee was", await pairContract.methods.swapFee().call());
         console.log("Changing swap fee to:", newSwapFee);
-        await factoryContract.methods
-            .setSwapFeeForPair(pairAddress, newSwapFee)
+
+        await timelockContract.methods
+            .queueTransaction(config.v2FactoryAddress, value,
+                                signature, data, eta)
             .send({ from: walletAddress })
-            .on("receipt", (receipt) => 
-            {
+            .on("receipt", (receipt) => {
                 transactionReceipt = receipt;
             });
 
-        console.log("Transaction Hash:", transactionReceipt.transactionHash);;
-        assert(pairContract.methods.swapFee() === newSwapFee);
+        const transactionHashQueued = transactionReceipt.   outputs[0].events[0].topics[1];
+        console.log("Transaction Hash of queued proposal:", transactionHashQueued);
+        
+        // Ensure that transaction is indeed queued
+        assert(await timelockContract.methods
+                        .queuedTransactions(transactionHashQueued)
+                        .call());
+
+        const output = {
+            timelockAddress: deployedAddresses.timelockAddress,
+            pairAddress: pairAddress,
+            signature: signature,
+            value: value,
+            data: data,
+            eta: eta,
+        }
+        fs.writeFileSync('./scripts/config/setSwapFee.json', JSON.stringify(output, null, 2));
     }
     catch(error)
     {
@@ -73,4 +100,4 @@ setSwapFee = async(pairAddress, newSwapFee) =>
     }
 }
 
-setSwapFee(pairAddress, swapFee);
+queueSetSwapFee(pairAddress, swapFee);
